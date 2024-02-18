2b87abf2c189c31ad8df34970cf0c4f53f16a9b7
Ryan James <ryanjames1729@gmail.com>
2024-02-18 10:54:02 -0500

    create form reading data - not synced with database yet

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,9 @@ export default function Home() {
   const [usernames, setUsernames] = useState([])
   const [readName, setReadName] = useState('')
 
+  const [createName, setCreateName] = useState('')
+  const [createScore, setCreateScore] = useState('')
+
 
   return (
     <>
@@ -45,6 +48,7 @@ export default function Home() {
             setDelete(false)
             setRead(false)
             create ? setRead(true) : setRead(false)
+            setUsernames([])
           }}/>
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Create | </span>
@@ -57,6 +61,7 @@ export default function Home() {
             setDelete(false)
             setRead(false)
             update ? setRead(true) : setRead(false)
+            setUsernames([])
           }}/>
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Update | </span>
@@ -69,6 +74,7 @@ export default function Home() {
             setDelete(!del)
             setRead(false)
             del ? setRead(true) : setRead(false)
+            setUsernames([])
           }}/>
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Delete | </span>
@@ -87,11 +93,21 @@ export default function Home() {
         </div>
 
         { create && (
-          <form className="flex flex-col items-center justify-center">
+          <form className="flex flex-col items-center justify-center" onSubmit={ (event) => {
+            event.preventDefault()
+            console.log(createName, createScore)
+          //   const names = getNames(createName) 
+          //   names.then((data: any) => {
+          //     setUsernames(data);
+          //     setCreateName('');
+          //     setCreateScore('');
+          // })
+        }
+          }>
             <label htmlFor="create" className="flex flex-col items-center justify-center">
               <span className="text-2xl font-bold text-center lg:text-4xl">Create</span>
-              <input type="text" name="createName" id="createName" placeholder="Create Name goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700"/>
-              <input type="text" name="createScore" id="createScore" placeholder="Create Score goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700"/>
+              <input type="text" name="createName" id="createName" placeholder="Create Name goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700 text-slate-700" value={createName} onChange={e => {setCreateName(e.target.value)}}/>
+              <input type="text" name="createScore" id="createScore" placeholder="Create Score goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700 text-slate-700" value={createScore} onChange={e => {setCreateScore(e.target.value)}}/>
               <button type="submit" className="w-96 h-12 mt-4 bg-blue-500 rounded-lg text-white">Create</button>
             </label>
           </form>
@@ -101,8 +117,8 @@ export default function Home() {
           <form className="flex flex-col items-center justify-center">
             <label htmlFor="create" className="flex flex-col items-center justify-center">
               <span className="text-2xl font-bold text-center lg:text-4xl">Update</span>
-              <input type="text" name="updateName" id="updateName" placeholder="Update Name goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700"/>
-              <input type="text" name="updateScore" id="updateScore" placeholder="Update Score goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700"/>
+              <input type="text" name="updateName" id="updateName" placeholder="Update Name goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700 text-slate-700"/>
+              <input type="text" name="updateScore" id="updateScore" placeholder="Update Score goes here" className="w-96 h-12 p-4 mt-4 border border-gray-300 rounded-lg dark:border-gray-700 text-slate-700"/>
               <button type="submit" className="w-96 h-12 mt-4 bg-blue-500 rounded-lg text-white">Update</button>
             </label>
           </form>
