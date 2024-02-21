89c4d06b48bae78759c8cbe5dcd07537fecc752f
Ryan James <ryanjames1729@gmail.com>
2024-02-21 15:21:22 -0500

    trying async function call on app page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-'use client'
+
 
 import Footer from "./components/Footer";
 import { useState } from "react";
-import { getNames, postName, mutateName, deleteName } from "./api/route";
+import { postName, mutateName, deleteName } from "./components/Results";
 
-import { use } from "react";
+import { GraphQLClient } from "graphql-request";
+
+async function getNames(name: string) {
+  const endpoint = process.env.GRAPHQL_PUBLIC_ENDPOINT;
+  const graphQLClient = new GraphQLClient(endpoint || '');
+  
+  const searchname = name ? name : "";
+  console.log("name", searchname)
+ 
+  const { usernames }: any = await graphQLClient.request(`
+  query Usernames($searchname: String!) {
+    usernames(where: {name_contains: $searchname}) {
+      id
+      name
+      points
+    }
+  }
+  `, { searchname }); // variables must be part of the request arguments!
+  console.log(usernames);
+  return usernames;
+}
 
 export default function Home() {
 
