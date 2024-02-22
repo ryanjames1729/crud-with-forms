4c080d4b3d82687e5e157a36b2f78d3f2bfd6f3c
Ryan James <ryanjames1729@gmail.com>
2024-02-22 17:03:22 -0500

    testing new route

---
+++ app/read/page.tsx
@@ -0,0 +1,11 @@
+import CreateForm from '../components/CreateForm';
+
+export default function Page() {
+
+    return (
+        <div>
+            <h1>Read or GET</h1>
+            <CreateForm />
+        </div>
+    )
+}
